9aa730d5bc3734af8d9d980e04f84ef1c624374d
Gervasio Marchand <github@gervas.io>
2022-12-20 23:10:42 -0300

    Use null reference checks (and enforce them)

--- src/MastodonConnectionHelper.cs
+++ src/MastodonConnectionHelper.cs
@@ -7,16 +7,19 @@ public static class MastodonConnectionHelper
     public static async Task<List<string>> GetFollowedTagsAsync()
     {
         var res = new List<string>();
+
+        if (Config.Instance == null)
+        {
+            throw new Exception("Config object is not initialized");
+        }
         
         await using var conn = new NpgsqlConnection(Config.Instance.MastodonPostgresConnectionString);
         await conn.OpenAsync();
 
-        await using (var cmd = new NpgsqlCommand("SELECT DISTINCT tags.name FROM tag_follows JOIN tags ON tag_id = tags.id ORDER BY tags.name ASC;", conn))
-        await using (var reader = await cmd.ExecuteReaderAsync())
-        {
-            while (await reader.ReadAsync())
-                res.Add(reader.GetString(0));
-        }
+        await using var cmd = new NpgsqlCommand("SELECT DISTINCT tags.name FROM tag_follows JOIN tags ON tag_id = tags.id ORDER BY tags.name ASC;", conn);
+        await using var reader = await cmd.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+            res.Add(reader.GetString(0));
 
         return res;
     }
